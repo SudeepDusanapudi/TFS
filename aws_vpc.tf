5e3bbb8bebcff3556868ffbb9237a258b04d6b12
Sudeep <Sudeepdusanapudi@gmail.com>
2023-10-30 19:13:46 +0530

    created subnets

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -7,3 +7,28 @@ resource "aws_vpc" "ecomm" {
     Name = "ecomm-vpc"
   }
 }
+
+#public_subnet
+resource "aws_subnet" "ecomm_public_sn" {
+  vpc_id     = aws_vpc.ecomm.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "ecomm_public_subnet"
+  }
+}
+
+
+#private_subnet
+resource "aws_subnet" "ecomm_private_sn" {
+  vpc_id     = aws_vpc.ecomm.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = "false"
+
+  tags = {
+    Name = "ecomm_private_subnet"
+  }
+}
